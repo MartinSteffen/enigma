9171497df0f6b5c472f7f60dce328b2ebc2335eb
Martin Steffen <msteffen@ifi.uio.no>
2017-07-18 10:32:46 +0200

    OK

--- enigma/io/FileIO.java
+++ enigma/io/FileIO.java
@@ -4,6 +4,7 @@
  * To change the template for this generated file go to
  * Window>Preferences>Java>Code Generation>Code and Comments
  */
+
 package enigma.io;
 import java.io.*;
 import java.util.jar.*;
